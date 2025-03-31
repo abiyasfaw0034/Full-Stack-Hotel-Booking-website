661da2a51612cf2afe3ab0d68b0076bceed8186e
abiy <abiyasfaw1996@gmail.com>
2025-03-31 13:25:00 -0700

    v1

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,14 @@
 function AboutUs() {
   return (
-    <div>
-      <div className="text-lg text-yellow-500 text-center">
-        about us is more than that
-      </div>
+    <div className=" p-20 text-center">
+      <h2 className="text-5xl font-bold my-8">About Us</h2>
+      <p className="text-3xl text-gray-700 dark:text-gray-300">
+        Welcome to our hotel booking platform, where finding your perfect stay
+        is simple and seamless. We connect travelers with top-rated hotels
+        worldwide, offering a hassle-free booking experience with the best
+        deals. Whether you're planning a business trip, a family vacation, or a
+        weekend getaway, we ensure comfort and convenience at every step.
+      </p>
     </div>
   );
 }
